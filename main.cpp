c9d076209aa0816fe27e8641c14131dd0c8f81be
Clément Jouin <95017825+ClementEcNantes@users.noreply.github.com>
2022-03-15 07:31:30 +0000

    Nothing

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class point {
+public 
+
+int point(int x, int y)
+  int abs = a;
+  int 
+
+int translater(int a, int b){
+  
+  
+}
+
+
+}
---
+++ utility.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class point{
+
+public 
+int point(int a, int b){
+  int x = a;
+  int 
+}
+
+int translater(int a, int b){
+  point 
+}
+
+}
